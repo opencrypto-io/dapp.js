c05f1b8ec55ec1035f5aebe9f3306492b690785e
Jan Stránský <jan.stransky@arnal.cz>
2018-08-13 09:40:19 +0200

    Basic Events

--- index.js
+++ index.js
@@ -25,22 +25,38 @@ class DAppClient {
     }
     this._services = {}
     this._provider = null
+    this._eventHandlers = {}
     this._debug = debug
 
-    // Load services
-    this._config.services.forEach(id => {
-      const servicePath = path.join(this._config.servicesDir, id)
-      this._services[id] = {
-        path: servicePath,
-        pkg: require(servicePath)
-      }
-    })
     // Load provider
     this._provider = new providers[this._config.provider.type](this)
+    // Load services
+    this.addServices(this._config.servicesDir, this._config.services)
     // Load account
     if (this._config.privateKey) {
       this._provider.addPrivateKeyAccount(this._config.privateKey)
     }
+  }
+
+  async addServices (dir, whitelist = null) {
+    return Promise.all(fs.readdirSync(dir).map(id => {
+      const servicePath = path.join(dir, id)
+      if (whitelist && whitelist.indexOf(id) === -1) {
+        return null
+      }
+      return this.addService(id, servicePath)
+    }))
+    .then(() => {
+      return this.emit('servicesLoaded')
+    })
+  }
+
+  async addService (id, path) {
+    this._services[id] = {
+      path,
+      pkg: require(path)
+    }
+    return Promise.resolve(this._services[id])
   }
 
   services () {
@@ -54,15 +70,12 @@ class DAppClient {
     })
   }
 
-  addService (name, path) {
-    this._services[name] = {
-      path,
-      pkg: require(path)
-    }
-  }
-
   async service (id) {
-    return new this._services[id].pkg.api(this, this._services[id], await this.assets(id))
+    await this.once('servicesLoaded')
+    if (!this._services[id].instance) {
+      this._services[id].instance = new this._services[id].pkg.api(this, this._services[id], await this.assets(id))
+    }
+    return this._services[id].instance
   }
 
   async assets (serviceId) {
@@ -86,7 +99,7 @@ class DAppClient {
         case 'abi':
           return service._assets.abi[id]
         case 'addr':
-          return service._index.contracts[this._config.network][id]
+          return service._index.pkg.contracts[this._config.network][id]
       }
     }
     Object.keys(args).forEach(prop => {
@@ -94,6 +107,39 @@ class DAppClient {
     })
     return this._provider.contract(args.abi, args.addr, opts)
   }
+
+  async emit (eventName) {
+    this._debug('Events')('Event emitted: ' + eventName)
+    if (!this._eventHandlers[eventName]) {
+      return Promise.resolve()
+    }
+    return Promise.all(this._eventHandlers[eventName].map(fn => fn()))
+  }
+
+  async on (eventName, fn = null, once = false) {
+    if (!this._eventHandlers[eventName]) {
+      this._eventHandlers[eventName] = []
+    }
+    let i = this._eventHandlers[eventName].length
+    return new Promise((resolve, reject) => {
+      this._eventHandlers[eventName].push((data) => {
+        if (once) {
+          if (i > 0) {
+            this._eventHandlers[eventName].splice(i, 1)
+          } else {
+            delete this._eventHandlers[eventName]
+          }
+        }
+        if (fn) fn()
+        resolve()
+      })
+    })
+  }
+
+  once (eventName, fn = null) {
+    return this.on(eventName, fn, true)
+  }
+
 }
 
 module.exports = {
