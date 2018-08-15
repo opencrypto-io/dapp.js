407bf151ba3759ae568effdf98cddda6abeaea3b
Jan Stránský <jan.stransky@arnal.cz>
2018-08-15 18:25:17 +0200

    Fix ERC20

--- services/erc20/index.js
+++ services/erc20/index.js
@@ -1,20 +1,6 @@
-const utils = require('../../lib/utils')
+const Service = require('../..').service
 
-class ERC20 {
-  constructor (api, index, assets) {
-    this._api = api
-    this._index = index
-    this._assets = assets
-  }
-
-  async token (addr) {
-    return new Token(this, addr)
-  }
-
-  async _call (addr, id, method, opts = []) {
-    const contract = await this._api.contract(this, id, {}, { addr })
-    return contract.methods[method].apply(null, opts).call()
-  }
+class ERC20 extends Service {
 
   async getDecimals (tokenAddr) {
     return this._call(tokenAddr, 'v1', 'decimals')
@@ -22,7 +8,7 @@ class ERC20 {
 
   async getSymbol (tokenAddr) {
     const symbol = await this._call(tokenAddr, 'v1', 'symbol')
-    return utils.web3utils.hexToAscii(symbol)
+    return this._utils.hexToAscii(symbol)
   }
 
   async getBalance (tokenAddr, owner) {
@@ -34,23 +20,13 @@ class ERC20 {
   }
 }
 
-class Token {
-  constructor (erc20, addr) {
-    this._parent = erc20
-    this.addr = Promise.resolve(addr)
-  }
-  async getAddr () {
-    return this.addr
-  }
-}
-
-utils.passthroughExpand(Token, 'getAddr', [
+ERC20.createChild('token', [
   'getDecimals',
   'getSymbol',
   'getBalance'
 ])
 
 module.exports = {
-  name: 'ERC-20 Tokens',
+  name: 'ERC-20 Token',
   api: ERC20
 }
---
+++ test/test-erc20.js
@@ -0,0 +1,15 @@
+
+async function test () {
+  const DApp = require('..')
+  const client = new DApp.client()
+  const erc = await client.service('erc20')
+
+  const token = erc.token('0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2')
+
+  console.log('symbol:', await token.getSymbol())
+  console.log('balance:', await token.getBalance('0xc1691ae1b3a19923ca4ea9d25176092f4112a7f1'))
+
+}
+
+test()
+
