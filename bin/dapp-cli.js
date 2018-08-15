7c1ff9ca074deecdd1d6a7f4fdc7035e2e78b400
Jan Stránský <jan.stransky@arnal.cz>
2018-08-15 19:39:27 +0200

    Basic cli commands

--- bin/dapp-cli.js
+++ bin/dapp-cli.js
@@ -3,7 +3,10 @@
 const DApp = require('..')
 const program = require('commander')
 
-program.usage('[options] <service> <method> <args ...>')
+program
+  .usage('[options] <service> <method> <args ...>')
+  .option('-p, --provider <type>', 'Use specified provider')
+  .option('-n, --network <name>', 'Use specified network')
 
 function err(msg) {
   console.error(msg)
@@ -15,11 +18,24 @@ async function cli(app) {
   if (!app.args[0]) {
     err('Please specify service')
   }
+  if (!app.args[1]) {
+    err('Please specify method')
+  }
+   
+  const opts = {}
+  if (app.network) {
+    opts.network = app.network
+  }
+  if (app.provider) {
+    opts.provider = {
+      type: app.provider
+    }
+  }
 
-  const client = new DApp.client()
+  const client = new DApp.client(opts)
   const service = await client.service(app.args[0])
 
-  console.log('Value:', await service[app.args[1]].apply(service, app.args.slice(2)))
+  process.stdout.write(await service[app.args[1]].apply(service, app.args.slice(2)) + "\n")
   
 }
 
