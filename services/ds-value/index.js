a1b4dbe47a2a7525d350517d59c16c0400881be8
Jan Stránský <jan.stransky@arnal.cz>
2018-08-17 21:34:27 +0200

    Update documentation

--- services/ds-value/index.js
+++ services/ds-value/index.js
@@ -1,10 +1,27 @@
 const Service = require('../../lib/service')
 
+/**
+ * ds-value - Set and get a value.
+ * Source: https://github.com/dapphub/ds-value
+ */
 class DSValue extends Service {
 
-  async set (addr, type = 'Hex') {
+  /**
+   * Set a value.
+   * @param {string} address - The address of target contract
+   * @param {string} [type=Hex] Format of the input (Hex, String, NumberString..)
+   * @returns {promise} Promise (resolves to transaction promise)
+   */
+  async set (addr, value, type = 'Hex') {
+    return this.$send(addr, 'DSValue', 'set', [value], { })
   }
 
+  /**
+   * Get a value.
+   * @param {string} address - The address of target contract
+   * @param {string} [type=Hex] Format of the results (Hex, String, NumberString..)
+   * @returns {promise} Promise (resolves to value)
+   */
   async get (addr, type = 'Hex') {
     const value = await this.$call(addr, 'DSValue', 'read')
     return this.$utils.formatHex(value, type)
